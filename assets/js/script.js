fbaf08bea53bb47073fee26a5ebb79078c915e09
AgustinG <agxtindos@gmail.com>
2025-11-30 18:34:54 -0300

    agregar ventana que pide nombre, correo y contraseña y validación de datos con js

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,3 +17,50 @@ form.addEventListener('submit', function(event) {
         form.reset();
     }
 });
+
+/* E-6 M-2 */
+/* Bloque de codigo para la ventana emergente que pide correo y contraseña */
+function solicitarDatos() {
+    //variables
+    let nombreCompleto, correoElectrónico, contraseña;
+
+    // Validar nombreCompleto
+    while (true) {
+        nombreCompleto = prompt("Ingresa tu nombre completo:");
+        if (nombreCompleto !== null && nombreCompleto.trim() !== "") {
+            break; // Si el campo no está vacío, salimos del ciclo
+        } else {
+            alert("Error: El campo 'nombre' no puede estar vacío.");
+        }
+    }
+
+    // Validar correo
+    while (true) {
+        correoElectrónico = prompt("Ingresa tu correo electrónico:");
+        if (correoElectrónico !== null && correoElectrónico.trim() !== "") {
+            break; // Si el campo no está vacío, salimos del ciclo
+        } else {
+            alert("Error: El campo 'correo' no puede estar vacío.");
+        }
+    }
+
+    // Validar contraseña
+    while (true) {
+        contraseña = prompt("Ingresa tu contraseña:");
+        if (contraseña !== null && contraseña.trim() !== "") {
+            if (contraseña.length >= 8) {
+                break; // Si la contraseña tiene al menos 8 caracteres, salimos del ciclo
+            } else {
+                alert("Error: La contraseña debe tener al menos 8 caracteres.");
+            }
+        } else {
+            alert("Error: El campo 'contraseña' no puede estar vacío.");
+        }
+    }
+
+    // Mensaje de victoria
+    alert(`✅ Registro exitoso. ¡Bienvenido, ${nombreCompleto} echa un vistazo y disfruta de la comunidad!`);
+}
+
+// llamado de la funcion
+solicitarDatos();
